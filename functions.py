2dc3783d1b3a9823e7496b2aec8e9679d956b1ea
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2023-11-22 00:30:34 +0000

    add error checking

--- functions.py
+++ functions.py
@@ -3,8 +3,12 @@
 # Returns a string that is the contents of the myName variable written backwards
 # ie: passing Atkinson as the parameter would return nosniktA
 def reverseName(myName):
-  # start with nothing
+    # start with nothing
   result = ''
+  # validate
+  if len(myName) <= 0 or myName == '':
+    return result
+
   # put the position finder at the end of the string
   pos = len(myName) - 1
   # loop backwards
@@ -17,10 +21,23 @@ def reverseName(myName):
 # Returns a float value that is the contents of the myAge variable square rooted
 # ie: passing 16 as the parameter would return 4.0
 def rootAge(myAge):
-  # import python math
+    # import python math and numbers
   import math
+  import numbers
+
+  result = 0.0
+  # validate
+  if len(myAge) <= 0 or myAge == '':
+    return result
+
+  if not isinstance(myAge, numbers.Number):
+    return result
+  
   # make sure the number is a number
   ageToSqrt = float(myAge)
+  if ageToSqrt <= 0:
+    return result
+  
   # get the square root
   result = math.sqrt(ageToSqrt)
   return result
